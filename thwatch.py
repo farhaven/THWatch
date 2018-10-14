f748248b384a707f21fed22c487e7b35bffa3846
Gregor Best <gbe@unobtanium.de>
2018-10-14 17:37:48 +0200

    Main: Watch new offers

--- thwatch.py
+++ thwatch.py
@@ -1,5 +1,7 @@
+import re
+import time
 import logging
-import random
+import sys
 
 import pushover
 import thwbus
@@ -8,6 +10,11 @@ import thwbus
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
+
+# Dict mapping regular expressions for offer titles to pushover user keys interested in them.
+ROUTING = {
+    re.compile(r'.*'): [pushover.PUSHOVER_USER_KEY]
+}
 
 
 def push_offer(target, offer):
@@ -18,10 +25,32 @@ def push_offer(target, offer):
         log.error("Failed to push message to %s: %s", target, res.text)
 
 
+def handle_new_offer(offer):
+    """ Handle signalling for a new offer, determine who's interested in that and push to them. """
+    for rkey, targets in ROUTING.items():
+        if rkey.match(offer.title):
+            for t in targets:
+                push_offer(t, offer)
+
+
 if __name__ == "__main__":
-    offers = thwbus.get_last_minute_offers()
-    for o in offers:
-        log.debug("%s", o)
-    push_offer(pushover.PUSHOVER_USER_KEY, random.choice(offers))
-    # pushover.push(text=f"Got {len(offers)} last minute offers",
-    #               title="New last minute offers at BuS")
+    known_offers = set(thwbus.get_last_minute_offers())  # Initialize set of known offers
+
+    log.debug("got %s known offers: %s", len(known_offers), known_offers)
+
+    while True:
+        print(sorted([hash(o) for o in known_offers]), len(known_offers))
+
+        offers = thwbus.get_last_minute_offers()
+        log.debug("Got %s offers", len(offers))
+
+        for o in offers:
+            if o in known_offers:
+                continue
+
+            log.debug("Got new offer %s", o)
+            known_offers.add(o)
+            # handle_new_offer(o)
+
+        log.debug("waiting 10 seconds before polling again.")
+        time.sleep(10)
