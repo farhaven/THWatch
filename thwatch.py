53e0c1403b2023573b33f862a7f9310c7d777bd6
Gregor Best <gbe@unobtanium.de>
2018-10-14 14:51:54 +0200

    Code init

---
+++ thwatch.py
@@ -0,0 +1,8 @@
+import requests
+
+
+baseurl = "https://www.thw-bundesschule.de/THW-BuS/DE/Ausbildungsangebot/Lehrgangskalender/lehrgangskalender_node.html?sort=lastMinute"
+
+
+if __name__ == "__main__":
+    pass
