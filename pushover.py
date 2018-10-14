29702c8be1d25108281c4ed165a8cd832941b15d
Gregor Best <gbe@unobtanium.de>
2018-10-14 15:50:59 +0200

    Import pushover code

---
+++ pushover.py
@@ -0,0 +1,48 @@
+""" This module deals with pushover notifications """
+import logging
+
+import requests
+
+PUSHOVER_API_TOKEN = "ax174e5v8i4hheyg1xgxqfw5uk8w92"
+PUSHOVER_USER_KEY = "FooBar"
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+class Priority:
+    """ Names for Pushover message priorites """
+    Lowest = -2  # No audible alert
+    Low = -1
+    Normal = 0
+    High = 1
+    Emergency = 2
+
+
+def _push(target, text, title, img=None, priority=Priority.Lowest):
+    reqdata = {
+        "token": PUSHOVER_API_TOKEN,
+        "user": target,
+        "title": title,
+        "message": text,
+        "priority": priority
+    }
+
+    if img is None:
+        r = requests.post("https://api.pushover.net/1/messages.json", data=reqdata)
+    else:
+        r = requests.post("https://api.pushover.net/1/messages.json",
+                          data=reqdata,
+                          files={"attachment": ("plot.png", img, "image/png")})
+
+    log.debug("Message to %s pushed: '%s', reply: %s", target, text, r)
+    return r
+
+
+log.debug("Done loading")
+
+if __name__ == "__main__":
+    log.debug("Testing push now.")
+    r = _push(PUSHOVER_USER_KEY, "This is a test", "Stockalert pushover test")
+    log.debug("Push done: %s: %s", r, r.text)
